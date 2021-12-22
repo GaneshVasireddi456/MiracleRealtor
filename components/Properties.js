24eba561180c7d8cce782ac1531e47baab0b64ad
GaneshVasireddi456 <ganeshvasireddi456@gmail.com>
2021-12-23 00:45:04 +0530

    xcvcxbv

--- components/Properties.js
+++ components/Properties.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'primereact/card';
 import Image from 'next/image'
 import '../styles/Home.module.css'
 import { Dialog } from 'primereact/dialog';
-import { useState } from 'react/cjs/react.development';
+
 
 
  const Properties = ({ data }) => {
@@ -16,7 +16,43 @@ import { useState } from 'react/cjs/react.development';
     }
     return (
         <div className="p-grid p-nogutter">
-           
+            {data.map((property) =>
+                <div key={property.id} className="p-col-12 p-lg-4 p-md-6 p-sm-12 card" onClick={() => { showDialog(property) }}>
+                    <Card  subTitle={<div className="p-d-flex p-jc-between" >
+                        <div style={{ fontSize: '16px', fontWeight: 'bold' }}><span>Price : </span><span style={{ fontWeight: 'bolder' }}>{property.price + " ₹"}</span></div>
+                        <div style={{ fontSize: '16px', fontWeight: 'bold' }}><span>No of rooms : </span><span style={{ fontWeight: 'bolder' }}>{property.rooms}</span></div>
+                    </div>}
+                        footer={<div className="p-d-flex p-jc-between">
+                            <div style={{ fontWeight: 'bold', color: '#4F46E5' }}>{property.agency.name}</div>
+                            <div ><Image src={property.agency.logo.url} width={'70px'} height={'30px'} /></div>
+                        </div>}
+                        className="p-m-2" >
+                        <Image src={property.coverPhoto.url} width={'500px'} height={'200px'} />
+                    </Card>
+                </div>)
+            }
+           {dialog && <Dialog modal='false' visible={dialog} onHide={() => setdialog(!dialog)} style={{ width: '60vw' }}  >
+                <div className="p-d-flex p-jc-center ">
+                    <div >
+                    <Image src={propertyData.coverPhoto.url?propertyData.coverPhoto.url:''} width={'500px'} height={'300px'} />
+                    </div>
+                    <div className="p-grid p-pt-2 p-pl-4">
+                       <p>
+                           <span style={{fontWeight:'bolder'}}>Area :</span>&nbsp;{Math.trunc(propertyData.area)}&nbsp;<span >sqft,</span>&nbsp;
+                            <span style={{fontWeight:'bolder'}}>Rooms :</span>&nbsp;{Math.trunc(propertyData.rooms)},&nbsp;
+                            <span style={{fontWeight:'bolder'}}>Baths :</span>&nbsp;{propertyData.baths},&nbsp;
+                            <span style={{fontWeight:'bolder'}}>Price :</span>&nbsp;{propertyData.price}&nbsp;₹,&nbsp;
+                            <span style={{fontWeight:'bolder'}}>Rent frequency :</span>&nbsp;{propertyData.rentFrequency},&nbsp;
+                            <span style={{fontWeight:'bolder'}}>Agency name :</span>&nbsp;{propertyData.agency.name},&nbsp;
+                            <span style={{fontWeight:'bolder'}}>Contact name :</span>&nbsp;{propertyData.contactName},&nbsp;
+                            <span style={{fontWeight:'bolder'}}>Mobile No :</span>&nbsp;{propertyData.phoneNumber.mobile},&nbsp;
+                            {/* <span style={{fontWeight:'bolder'}}>Location :</span>&nbsp;[{propertyData._geoloc.lat},{propertyData._geoloc.lng}],&nbsp; */}
+                            </p>
+                    </div>
+                    </div>
+
+
+            </Dialog>}
         </div>
     )
 }
